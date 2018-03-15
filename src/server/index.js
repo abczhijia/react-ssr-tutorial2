0ed1bdc0eb213d194c007e9b8601868b917184e7
abczhijia <abczhijia@gmail.com>
2018-03-15 20:28:24 +0800

    2、koa配置静态文件和路由，并引入浏览器端打包文件client.js，结合koa，运行react的helloworld应用

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,27 @@
 import Koa from 'koa';
+import KoaStatic from 'koa-static';
+import KoaRouter from 'koa-router';
 
 const app = new Koa();
+const router = new KoaRouter();
 
-app.use((ctx, next) => {
-    ctx.body = 'hello server';
+router.get('*', (ctx, next) => {
+    ctx.body = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="/client.js"></script>
+    </body>
+    </html>`;
 });
+
+app.use(KoaStatic('build'));
+app.use(router.routes());
 
 app.listen(3000, () => {
     console.log('server is listening on 3000');
